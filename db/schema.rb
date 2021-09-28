e108f9650fdc66ee6daf8b8e950a346e9dc84c76
Rafael Beraldo <umtalderafael@hotmail.com>
2021-09-28 11:19:51 -0300

    retorno para a branch main

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_135354) do
+ActiveRecord::Schema.define(version: 2021_09_24_134306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,10 @@ ActiveRecord::Schema.define(version: 2021_09_21_135354) do
   create_table "users", force: :cascade do |t|
     t.string "name", limit: 50
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "born_at"
   end
 
 end
